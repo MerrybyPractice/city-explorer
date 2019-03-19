68902ff614f3e0ecd37d09ac11106006680ec7e8
Merry Cimakasky <mkcimakasky@gmail.com>
2019-03-19 12:30:53 -0700

    geo data fix

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 'use strict';
 
+//server architecture
+
 require('dotenv').config();
 
 const express = require('express');
-const app = express(); 
+const app = express();
 
 const cors = require('cors');
 app.use(cors());
 
 const PORT = process.env.PORT;
+const MAPS_API = process.env.MAPS_API;
+
+//pathing
 
 app.get('/location', (request, response) => {
   const locationData = searchToLatLong(request.query.data);
   response.send(locationData);
-})
+});
+
 
 app.get('/testing', (request, response) => {
   console.log('Im here.');
-  let test = {test: `this works on PORT${PORT}`
+  const test = {test: `this works on PORT${PORT}`}
+  response.sendStatus(test);
 });
-app.listen(PORT, () => console.log('Listening on PORT ${PORT}'));
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+
+//helper functions
+
+function searchToLatLong(query){
+
+  const geoData = require('./geo.json');
+  const location = new Location(geoData);
+  location.search_query = query;
+  console.log(location);
+  return location;
+}
+
+function Location(data) {
+  this.formatted_query = data.results[0].formatted_address;
+  this.latitude = data.results[0].geometry.location.lat;
+  this.longitude = data.results[0].geometry.location.lng;
+}
