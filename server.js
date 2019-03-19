c85d0ec1a6e900e0437cd4f56d296a48c41e57c2
Merry Cimakasky <mkcimakasky@gmail.com>
2019-03-19 13:33:49 -0700

    finishing

--- server.js
+++ server.js
@@ -11,14 +11,18 @@ const cors = require('cors');
 app.use(cors());
 
 const PORT = process.env.PORT;
-const MAPS_API = process.env.MAPS_API;
+//const MAPS_API = process.env.MAPS_API;
 
 //pathing
-
 app.get('/location', (request, response) => {
   const locationData = searchToLatLong(request.query.data);
   response.send(locationData);
 });
+
+app.get('/weather', (request, response) => {
+  const weatherData = searchWeather(request.query.data);
+  response.send(weatherData);
+})
 
 
 app.get('/testing', (request, response) => {
@@ -27,9 +31,13 @@ app.get('/testing', (request, response) => {
   response.sendStatus(test);
 });
 
+
+
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
 //helper functions
+
+//location
 
 function searchToLatLong(query){
 
@@ -40,8 +48,28 @@ function searchToLatLong(query){
   return location;
 }
 
-function Location(data) {
+function Location(data){
   this.formatted_query = data.results[0].formatted_address;
   this.latitude = data.results[0].geometry.location.lat;
   this.longitude = data.results[0].geometry.location.lng;
 }
+
+//weather
+
+function searchWeather(query){
+  if(Location.latitude === 'latitude' && Location.longitude === 'longitude'){
+    const darkSky = require('./darksky.json');
+    const weather = new Weather(darkSky);
+    weather.search_query = query;
+    console.log(weather);
+    return weather;
+  }
+}
+
+
+function Weather(data){
+  this.forecast = data.results[0].daily.data.summary;
+  this.time = data.results[0].daily.data.time.toString();
+
+}
+
